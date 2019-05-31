4c2ec74da1f14a11c1b6c3f850b86cb4773e8411
Taras Rushchak <taraskhust@gmail.com>
2019-05-31 15:15:36 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,66 @@
 import React from 'react';
 import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
+
+
+const Fake = ({ className }) => <div className={className}>
+  <h2> I'm a fake component </h2>
+</div>
+
+const Heading = styled.h1 `
+  font-size: 2rem;
+`
+
+const color = "white";
+const Button = styled.button `
+  color: ${color};
+  padding: 25px 20px;
+  border-radius: 5px;
+  font-weight: bold;
+  background: indigo;
+`
+const CancelButton = styled(Button) `
+  color: ${color};
+  background: aqua;
+  padding: 10px;
+`
+
+const AppWrapper = styled.div `
+  background: #61dafb;
+  header {
+    background: teal;
+      &:hover {
+      background: red;
+    }
+  }
+  img {
+  width: 500px;
+  }
+  ${Button} {
+  margin-bottom: 2rem;
+  }
+`
+
+const DoubleFake = styled(Fake)`
+    h2 {
+    color: red;
+    }
+`
 
 function App() {
   return (
-    <div className="App">
+    <AppWrapper>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Heading className='App'>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </Heading>
+        <DoubleFake>
+          <Fake/>
+        </DoubleFake>
+        <Button type="okay">Save</Button>
+        <CancelButton>Cancel</CancelButton>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,8 +70,9 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </AppWrapper>
   );
 }
+
 
 export default App;
